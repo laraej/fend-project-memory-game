b9b769a369edd928518946a7e74e69ba40d7b8da
Lara <lara.ej@gmail.com>
2018-07-25 20:37:54 +0100

    Adding deck shuffling at start

--- js/app.js
+++ js/app.js
@@ -36,3 +36,50 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+function shuffleDeck() {
+
+    // All unique cards.
+    var uniques = [
+        'fa-anchor',
+        'fa-bicycle',
+        'fa-bolt',
+        'fa-bomb',
+        'fa-cube',
+        'fa-diamond',
+        'fa-leaf',
+        'fa-paper-plane-o'
+    ];
+
+    // All cards, each unique card twice.
+    var cards = uniques.concat(uniques);
+
+    shuffle(cards);
+
+    var ul = document.getElementById("deck");
+
+    // Remove all content from the element.
+    ul.innerHTML = '';
+
+    for (var i = 0; i < cards.length; i++) {
+        var li = createCard(cards[i]);
+
+        ul.appendChild(li);
+    }
+}
+
+function createCard(name) {
+    var i = document.createElement("i");
+    i.setAttribute("class", "fa " + name);
+
+    var li = document.createElement("li");
+    li.setAttribute("class", "card match");
+    li.appendChild(i);
+
+    return li;
+}
+
+// Shuffle deck at start.
+window.onload = function() {
+    shuffleDeck();
+};
